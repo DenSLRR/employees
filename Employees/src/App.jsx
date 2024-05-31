b5f445b89c283c54fda5f4cead465cf3e4ab1c4a
Denis <denslrrwork@gmail.com>
2024-05-31 03:52:33 +0300

    up state

--- Employees/src/App.jsx
+++ Employees/src/App.jsx
@@ -19,18 +19,21 @@ class App extends Component {
 					name: 'John C.',
 					salary: 500,
 					increase: true,
+					rise: false,
 					id: 1,
 				},
 				{
 					name: 'Artur S.',
 					salary: 3000,
 					increase: false,
+					rise: false,
 					id: 2,
 				},
 				{
 					name: 'Ahmed C.',
 					salary: 4000,
 					increase: false,
+					rise: false,
 					id: 3,
 				},
 			],
@@ -46,34 +49,81 @@ class App extends Component {
 		})
 	}
 
-addItem = (name, salary) => {
-  const newItem = {
-    name,
-    salary,
-    increase: false,
-    id: this.maxId++
-  }
+	addItem = (name, salary) => {
+		const newItem = {
+			name,
+			salary,
+			increase: false,
+			rise: false,
+			id: this.maxId++,
+		}
 
-  this.setState(({data}) => {
-    const newArr = [...data, newItem];
-    return {
-      data: newArr
-    }
-  })
+		
+
+		this.setState(({ data }) => {
+			const newArr = [...data, newItem]
+			return {
+				data: newArr,
+			}
+		})
+	}
+
+	onToggleIncrease = id => {
+		// this.setState(({data}) => {
+		// 	const index = data.findIndex(elem => elem.id === id);
+
+		// 	const old = data[index];
+		// 	const newItem = {...old, increase: !old.increase};
+		// 	const newArr = [...data.slice(0, index), newItem, ...data.slice(index + 1)];
+
+		// 	return {
+		// 		data: newArr
+		// 	}
+		// })
+
+		this.setState(({data}) => ({
+			data: data.map(item => {
+				if (item.id === id) {
+					return {...item, increase: !item.increase}
+				}
+			  return item;
+			})
+		}))
+
+	}
 
 
-}
+	
+
+	onToggleRise = id => {
+		this.setState(({data}) => ({
+			data: data.map(item => {
+				if (item.id === id) {
+					return {...item, rise: !item.rise}
+				}
+			  return item;
+			})
+		}))
+	}
 
 	render() {
+
+		const employees = this.state.data.length;
+		const increased = this.state.data.filter(item => item.increase).length;
+
 		return (
 			<div className='app'>
-				<AppInfo />
+				<AppInfo employees={employees} increased={increased}/>
 
 				<div className='search-panel'>
 					<SearchPanel />
 					<AppFilter />
 				</div>
-				<EmployeesList onDelete={this.deleteItem} data={this.state.data} />
+				<EmployeesList 
+					onDelete={this.deleteItem} 
+					data={this.state.data}
+					onToggleIncrease={this.onToggleIncrease}
+					onToggleRise={this.onToggleRise} />
 				<EmployeesAddForm onAdd={this.addItem} />
 			</div>
 		)
--- Employees/src/components/employees-list-item/employees-list-item.jsx
+++ Employees/src/components/employees-list-item/employees-list-item.jsx
@@ -1,47 +1,26 @@
 import './employees-list-item.css';
-import { Component } from 'react';
 
-class EmployeesListItem extends Component {
-    constructor(props) {
-        super(props);
-    
-        this.state = {
-            increase: false,
-            like: false
-        }
-    }
-    
-    onIncrease = () => {
-        this.setState(({increase}) => ({
-            increase: !increase
-        }))
-    }
 
-    onLike = () => {
-        this.setState(({like}) => ({
-            like: !like
-        }))
-    }
+const EmployeesListItem = (props) => {
 
-    render() {
-
-        const {name, salary, onDelete} =  this.props;
-        const {increase, like} = this.state;
+ 
+        const {name, salary, onDelete, onToggleIncrease, onToggleRise, increase, rise} = props;
+        
 
         let classNames ='list-group-item d-flex justify-content-between ';
         if (increase) {
             classNames += ' increase';
-        } if (like) {
+        } if (rise) {
             classNames += ' like';
         }
         return (
             <li className={classNames}>
-                <span onClick={this.onLike} className= 'list-group-item-label'>{name}</span>
+                <span onClick={onToggleRise} className= 'list-group-item-label'>{name}</span>
                 <input type="text" className="list-group-item-input" defaultValue={salary + '$'}/>
                 <div className='d-flex justify-content-center align-items-center'>
                     <button type="button"
                         className="btn-cookie btn-sm " 
-                            onClick={this.onIncrease}>
+                            onClick={onToggleIncrease}>
                         <i className="fas fa-cookie"></i>
                     </button>
     
@@ -54,6 +33,6 @@ class EmployeesListItem extends Component {
             </li>
         )
     }
-}
+
 
 export default EmployeesListItem;
